10e16f9649d63e49f97d7262d8ebcaf05c726476
ziitania <tjhaweszii@gmail.com>
2021-03-15 13:09:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
+let num1 = 31;
+let num2 = 2;
+let num3 = 40;
+let num4 = 6;
+
 // Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(1 + (Math.random() * 10));
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 20);
